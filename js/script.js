5061200293c24dbb8123735028478f0f00eb3329
Mauro0Z <mauro.zm02@gmail.com>
2024-05-09 20:03:56 -0400

    v.1

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+document.getElementById("imcForm").addEventListener("submit",function(event){
+    
+    event.preventDefault(); //evita que el formulario se envie de forma predeterminada
+    
+    let altura = parseFloat(document.getElementById("altura").value);
+    const peso = parseFloat(document.getElementById("peso").value);
+    const resultadoIMC = document.getElementById("resultadoIMC");
+
+        
+    if (altura === "" || peso === ""){
+        alert("Ingrese valores");
+    }
+
+    altura = altura / 100;
+
+    const IMC = peso / (altura * altura);
+    resultadoIMC.textContent = IMC.toFixed(2); //Limita el resultado a 2 decimales
+
+});
